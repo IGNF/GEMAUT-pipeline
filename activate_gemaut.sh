40344bba672a4f0f6857d08311171c643ad68e55
champignon <nicolas.champion@ign.fr>
2025-02-05 16:08:52 +0100

    install conda

--- activate_gemaut.sh
+++ activate_gemaut.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+# Définition des chemins d'installation
 export SAGA_INSTALL_DIR=${SAGA_INSTALL_DIR:-$HOME/GEMAUT/saga_install}
 export GEMAUT_INSTALL_DIR=${GEMAUT_INSTALL_DIR:-$HOME/GEMAUT/GEMO}
 
+# Configuration des bibliothèques
 export LD_LIBRARY_PATH=$SAGA_INSTALL_DIR/lib:$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 export PROJ_LIB=$CONDA_PREFIX/share/proj
+
+# Ajouter les binaires de SAGA et GEMAUT au PATH
 export PATH=$SAGA_INSTALL_DIR/bin:$GEMAUT_INSTALL_DIR/bin:$PATH
 
+# Ajouter script_gemaut.py dans le PATH via un lien symbolique
+if [ ! -f "$CONDA_PREFIX/bin/script_gemaut" ]; then
+    ln -sf $HOME/GEMAUT/script_gemaut $CONDA_PREFIX/bin/script_gemaut
+fi
+
+# Ajouter script_gemaut.py dans le PATH via un lien symbolique
+#if [ ! -f "$CONDA_PREFIX/bin/script_gemaut" ]; then
+#    ln -sf $GEMAUT_INSTALL_DIR/script_gemaut.py $CONDA_PREFIX/bin/script_gemaut
+#fi
+
 echo "GEMAUT and SAGA environment activated."
+
--- deactivate_gemaut.sh
+++ deactivate_gemaut.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Suppression des variables d'environnement
+unset SAGA_INSTALL_DIR
+unset GEMAUT_INSTALL_DIR
 unset LD_LIBRARY_PATH
 unset PROJ_LIB
 
@@ -9,8 +11,11 @@ if [[ -n "$SAGA_INSTALL_DIR" ]]; then
     export PATH=${PATH//"$SAGA_INSTALL_DIR/bin:"/}
 fi
 if [[ -n "$GEMAUT_INSTALL_DIR" ]]; then
-    export PATH=${PATH//":$GEMAUT_INSTALL_DIR/bin"/}
+    export PATH=${PATH//"$GEMAUT_INSTALL_DIR/bin:"/}
 fi
+
+# Suppression du lien symbolique de script_gemaut.py
+rm -f $CONDA_PREFIX/bin/script_gemaut
 
 echo "GEMAUT and SAGA environment deactivated."
 
