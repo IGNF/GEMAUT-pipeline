c1a83e5079399e1da4d26c98464b8a4ef4c1b664
champignon <nicolas.champion@ign.fr>
2025-03-03 13:45:27 +0100

    nettoyage 3/3

--- deactivate_gemaut.sh
+++ deactivate_gemaut.sh
@@ -10,12 +10,15 @@ unset PROJ_LIB
 if [[ -n "$SAGA_INSTALL_DIR" ]]; then
     export PATH=${PATH//"$SAGA_INSTALL_DIR/bin:"/}
 fi
+
 if [[ -n "$GEMAUT_INSTALL_DIR" ]]; then
     export PATH=${PATH//"$GEMAUT_INSTALL_DIR/bin:"/}
 fi
 
 # Suppression du lien symbolique de script_gemaut.py
 rm -f $CONDA_PREFIX/bin/script_gemaut
+
+rm -f $CONDA_PREFIX/bin/saga_cmd
 
 echo "GEMAUT and SAGA environment deactivated."
 
