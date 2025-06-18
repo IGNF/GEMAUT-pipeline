0043f64cc5274a414ed149856fa6a0f6fee79066
Nicolas Champion <nicolas.champion@ign.fr>
2025-06-18 10:29:44 +0200

    ajout activate_gemaut.sh deactivate_gemaut.sh qui manquaient

---
+++ deactivate_gemaut.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Suppression des variables d'environnement
+unset SAGA_INSTALL_DIR
+unset GEMAUT_INSTALL_DIR
+unset LD_LIBRARY_PATH
+unset PROJ_LIB
+
+# Suppression propre des chemins ajoutés au PATH
+if [[ -n "$SAGA_INSTALL_DIR" ]]; then
+    export PATH=${PATH//"$SAGA_INSTALL_DIR/bin:"/}
+fi
+
+if [[ -n "$GEMAUT_INSTALL_DIR" ]]; then
+    export PATH=${PATH//"$GEMAUT_INSTALL_DIR/bin:"/}
+fi
+
+# Suppression du lien symbolique de script_gemaut.py
+rm -f $CONDA_PREFIX/bin/script_gemaut
+
+rm -f $CONDA_PREFIX/bin/saga_cmd
+
+echo "GEMAUT and SAGA environment deactivated."
+
